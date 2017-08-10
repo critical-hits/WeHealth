2cf536cf38bdf196ac54bc98e320fb72e53698dd
critical-hits <245304321@qq.com>
2017-08-10 14:56:56 +0800

    update index

---
+++ WeHealth.py
@@ -0,0 +1,21 @@
+import requests
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        do_the_login()
+    else:
+        show_the_login_form()
+
+
+if __name__ == '__main__':
+    app.run()

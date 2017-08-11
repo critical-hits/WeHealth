799072d75980504be458d55923dc18a1e5240b2d
Feeling-Jiang <597445175@qq.com>
2017-08-11 15:59:41 +0800

    connected

--- Connect.py
+++ Connect.py
@@ -1,5 +1,9 @@
 import socket
 
+#约定
+# 8001~8007为七个进程数据通信的端口号
+
+#给指定ip主机的进程发送数据
 def sendData(ip,port,data):
     address = (ip, port)
     client = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
@@ -8,3 +12,23 @@ def sendData(ip,port,data):
     data = client.recv(1024)
     print data
     client.close()
+
+#各个进程都时刻监控自身指定的端口，一旦有数据过来，立马接收
+def recData(ip,port=8000):
+    #ip为该进程所在主机的ip地址，默认prot=8000
+    address = (ip, port)
+    server = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind(address)
+    server.listen(7)
+
+    while True:
+        conn, addr = server.accept()
+        while True:
+            data = conn.recv(1024)
+            print data
+            # conn.sendall(data)
+            if not data:
+                conn.close()
+                break
+    server.close()

04656361f9e76f3a5ee15be316b46f634de586e0
critical-hits <245304321@qq.com>
2017-08-12 00:14:44 +0800

    update send

--- Connect.py
+++ Connect.py
@@ -2,8 +2,8 @@ import socket
 
 #给指定ip主机的进程发送数据
 def sendData(data,ip,port=8000):
-    address = ('10.39.167.101', 8001)
+    address = (ip, port)
     client = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
     client.connect(address)
-    client.send('jianghezhi,client')
+    client.send(data+',client')
     client.close()

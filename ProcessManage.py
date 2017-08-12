6cad293fab42ea62a3ff6edcd1520cd0e740d969
Gabriel Jiang <597445175@qq.com>
2017-08-12 20:22:24 +0800

    Merge branch 'master' of https://github.com/critical-hits/WeHealth
    # Conflicts:
    #	Connect.py

--- ProcessManage.py
+++ ProcessManage.py
@@ -1,13 +1,41 @@
+import socket
 import threading
-def sayHello():
-    print "Hello"
-    t=threading.Timer(3,sayHello)
-    t.start()
 
+class ProcessManage(object):
+    def __init__(self,addresses):
+        t = threading.Timer(30, self.sync(addresses))
+        t.start()
 
-def other_func():
-    print "Other"
+    def sync(self,addresses):
+        data = 'synchronized#sync'
+        client = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+        for i in range(0,len(addresses)):
+            client.connect(addresses[i])
+            client.send(data)
+            client.close()
 
 if __name__=="__main__":
-    sayHello()
-    other_func()
+    '''
+        ips = ['192.168.43.235', '192.168.43.113', 'localhost']
+        ports = [8001, 8002, 8003]
+        # 七个进程的位置
+        address1 = (ips[0], ports[0])
+        address2 = (ips[0], ports[1])
+        address3 = (ips[1], ports[0])
+        address4 = (ips[1], ports[1])
+        address5 = (ips[2], ports[0])
+        address6 = (ips[2], ports[1])
+        address7 = (ips[2], ports[2])
+        '''
+    ips = ['192.168.43.113', 'localhost']
+    ports = [8001, 8002, 8003, 8004]
+    # 七个进程的位置
+    address1 = (ips[0], ports[0])
+    address2 = (ips[0], ports[1])
+    address3 = (ips[0], ports[2])
+    address4 = (ips[1], ports[0])
+    address5 = (ips[1], ports[1])
+    address6 = (ips[1], ports[2])
+    address7 = (ips[1], ports[3])
+    addresses = [address1, address2, address3, address4, address5, address6, address7]
+    pm = ProcessManage(addresses)
